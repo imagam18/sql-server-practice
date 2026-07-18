57af11d45c4d336b8eb6b3433bade9b82a1d24f4
Agam saxena <agamsaxena80@gmail.com>
2026-07-18 11:42:24 +0530

    Add files via upload

---
+++ OR-AND.sql
@@ -0,0 +1,12 @@
+
+
+SELECT * FROM Employees
+WHERE LastName = 'saxena' and Employee = 5;
+
+SELECT * FROM Employees
+WHERE LastName = 'saxena' and Employee = 5;
+
+USE Employee_Details
+SELECT * FROM Employees
+
+WHERE Deparmtment = 'HR' OR  Deparmtment = 'IT'

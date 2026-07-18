ff480a5d37c906a64d045f924d0bc337dd9358c8
Agam saxena <agamsaxena80@gmail.com>
2026-07-18 11:33:42 +0530

    Add files via upload

---
+++ OrderBy.sql
@@ -0,0 +1,22 @@
+
+
+SELECT * FROM [dbo].[Employees]
+
+DELETE FROM Employees
+WHERE Employee = 6 OR Employee = 101;
+
+SELECT * FROM [dbo].[Employees]
+ORDER BY Salary
+
+SELECT * FROM [dbo].[Employees]
+ORDER BY FirstName
+
+SELECT * FROM [dbo].[Employees]
+ORDER BY Salary DESC
+
+SELECT * FROM [dbo].[Employees]
+ORDER BY FirstName asc , Salary desc , Deparmtment asc
+
+SELECT * FROM [dbo].[Employees]
+ORDER BY FirstName asc , Salary desc 
+

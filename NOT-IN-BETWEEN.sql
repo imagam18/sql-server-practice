7b63bd337540c3128509a7bf6bb98d54dc2dbf8c
Agam saxena <agamsaxena80@gmail.com>
2026-07-18 11:54:53 +0530

    Add files via upload

---
+++ NOT-IN-BETWEEN.sql
@@ -0,0 +1,29 @@
+
+
+
+SELECT * FROM dbo.Employees
+
+
+SELECT * FROM dbo.Employees
+WHERE not FirstName ='Rahul' AND not Salary = 60000
+
+
+SELECT * FROM dbo.Employees
+WHERE not LastName = 'Singh' or not Deparmtment ='HR'
+
+SELECT * FROM dbo.Employees
+
+SELECT * FROM dbo.Employees
+WHERE Salary between 50000 and 55000
+
+SELECT * FROM dbo.Employees
+WHERE Salary  not between 50000 and 55000
+
+SELECT * FROM dbo.Employees
+WHERE not Salary between 50000 and 55000
+
+SELECT * FROM dbo.Employees
+WHERE Deparmtment = 'HR' or Deparmtment = 'IT'
+SELECT * FROM dbo.Employees
+
+
